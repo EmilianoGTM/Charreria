a72696bede7262a9014ac6d94e2b24537c69d9d4
nano <elnanopro09@gmail.com>
2022-03-02 13:49:12 -0600

    first commit

---
+++ Ejercicio 1.php
@@ -0,0 +1,176 @@
+<html>
+
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@700&display=swap" rel="stylesheet">
+<title>La Charreria,historia,atuendos y reglamentos</title>
+<link rel="shortcut icon" href="mariachi.png">
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<meta charset="utf-8">
+
+</head>
+
+<body>
+	<div class="hero">
+		<div class="tittle">
+			<h1 class="float">Charreria </h1>
+			<br>
+			<h3 class="float2">deporte de la fiesta brava</h3>
+		</div>
+	</div>
+	
+<div class="container">
+	
+<div class="space"></div>
+
+<h2>¿Que es la charreria?</h2>
+
+<p align="center">La Charrería es un arte y disciplina surgida de las actividades ecuestres y tradiciones ganaderas. El estado de Hidalgo es considerado “la cuna de la charrería”. Fue catalogado como un deporte, gracias a la Reforma Agraria de principios del S. XX.
+</p>
+
+<h2>Historia</h2>
+<p>
+Tiene sus raíces en los estados de Hidalgo y Jalisco, convirtiéndose en una de las tradiciones mexicanas más representativas de nuestra cultura. Como escuela Hípica, se origina en los llanos de Apán en Hidalgo y tras pasar la Revolución mexicana a principios del Siglo XX, se le considera como Deporte Nacional cuando los antiguos terratenientes emigran a ciudades como la Ciudad de México y Guadalajara, donde se comienzan organizar en asociaciones que gradualmente se extendieron por todo el país dando lugar a la Charrería Organizada.
+</p>
+<h2>Vestimenta en la Charrería</h2>
+
+<p>
+Los orígenes del traje de charro, provienen de la misma actividad en el campo, desde que a los
+nativos de la Nueva España, se les permitió montar a caballo y realizar las faenas propias de
+sus actividades.
+ Traje de faena: Se compone de sombrero de palma, camisa sin cuello, saco de tela o
+gamuza sin adornos. Pantalón sin bolsa trasera, botines de piel y el general se utilizan
+colores en tonalidades café o gris, corbata discreta de moño colgante estilo mariposa.
+ Traje de media gala: Sombrero de fieltro, camisa de cuello volteado, corbata elegante
+haciendo juego con el traje, chaqueta de gamuza o casimir con tres presillas en cada
+manga y broche lujoso en el pecho. Pantalón con botonadura de plata en cada lado.
+Cinturón con adornos de acuerdo a los colores y materiales de todo el conjunto.
+ Traje de gala: De color gris, azul obscuro o negro, con sombrero galoneado y
+botonadura de plata desde la cintura hasta el tobillo, exige adornos discretos pero
+finos, pistola con funda bordada. Camisa de cuello volteado y chaqueta del mismo
+material. Corbata discreta con bordados finos de preferencia plateados haciendo juego
+con la botonadura y el sombrero.
+</p>
+
+<h2>Reglamento</h2>
+
+<p>
+
+En el reglamento de este deporte puede variar, ya que dentro de la charrería hay variaciones de edades y de ejercicios como puede ser: Escaramuzas, damas charras, charros infantiles, juveniles, mayores y categoría libre que incluye todas las edades.
+Pero el reglamento en general es parecido en todos excepto con las escaramuzas
+
+Es una competencia que puede desarrollarse en distintas modalidades, bien sea individual o por equipos, y/o en distintas categorías.
+Por equipos, consiste en realizar las faenas charras que son nueve y se integran de diez
+suertes: la cala de caballo, los píales en la manga del lienzo, el coleadero, el jineteo de toro,
+la terna en el ruedo (lazo de cabeza y lazo de pial), el jineteo de yegua, las manganas a pie,
+las manganas a caballo y el paso de la muerte. En estas, podrán competir dos o tres equipos y
+también un equipo o dos de escaramuza charra, según las características con las que sea
+convocada la charreada, y podrán ser en las categorías infantil, juvenil, adultos y mayores.
+
+Las competencias individuales de charro completo, consisten en que un solo charro
+competidor, realice las suertes de cala de caballo, píales en la manga del lienzo, coleadero,
+jineteo de toro, manganas a pie, manganas a caballo y paso de la muerte; en ésta modalidad,
+de competencias de charro completo, también podrán participar uno o dos equipos de
+escaramuza charra, o grupos de Escaramuzas que se denominaran feria de escaramuzas,
+según se convoque y podrán ser en las categorías de infantil, juvenil, adultos y mayores. 
+Las competencias individuales de charro mayor, consisten en que un solo charro competidor de
+ésta categoría (de cuarenta y cinco años de edad o más) realice las suertes de cala de caballo,
+píales en la manga del lienzo, coleadero, manganas a pie y manganas a caballo. En éste tipo
+de Charreadas, también podrán participar uno o dos equipos de escaramuza charra según se
+establezca en la convocatoria respectiva.
+Para más información puedes visitar el sitio web del gobierno mexicano donde se encuentra aun más extendido y con mayor precisión el reglamento.
+
+</p>
+<div class="center">
+		<button>
+		<a href="https://fmcharreria.org.mx/reglamentos/reglamento-oficial-competencias-varonil-libre-y-juvenil-2022/">Ver Reglamento</a>
+	</button>
+</div>
+
+<div class="space"></div>
+
+<h2>Las 9 suertes en la charrería</h2>
+
+<p>
+Las 9 suertes se dividen en las siguientes representaciones: la Calada de Caballos, Piales en el Lienzo, Coleadero, Jineteo del Toro, la Faena de la Terna en el Ruedo, Jineteo de Yegua, Manganas a Pie, Manganas a Caballo, Paso de la Muerte. 
+</p>
+
+<h2>Cala de Caballo:</h2>
+
+<p>Consiste en demostrar la buena rienda del equino. Comienza con la punta, que es detener el caballo a todo galope partiendo desde el fondo del partidero, dentro de un rectángulo de 20 x 6 metros.</p>
+
+<div class="flex">
+	<img src="./img/caladecaballo/cala1.jpg">
+	<img src="./img/caladecaballo/cala2.jpg">
+	<img src="./img/caladecaballo/cala3.jpg">
+</div>
+
+<h2>Piales:</h2>
+
+<p>consiste en detener la carrera de una yegua lazándole la reata a sus cuartos traseros. Se trata de una bella combinación entre el manejo del caballo y la destreza del jinete con la reata.</p>
+<div class="flex">
+	<img src="./img/caladecaballo/cala1.jpg">
+	<img src="./img/caladecaballo/cala2.jpg">
+	<img src="./img/caladecaballo/cala3.jpg">
+</div>
+<h2>Coleadero:</h2>
+
+<p>
+	consiste en intentar derribar un toro pequeño por su cola mientras galopa y derribarlo lo mayor antes posible.
+</p>
+<h2>
+	Jineteo de toro:
+</h2>
+<p>
+	consiste en la monta de toros. El objetivo es que el jinete permanezca montado sobre un toro hasta que deje de moverse para después desmontar.
+</p>
+<h2>
+	Terna en el ruedo:
+</h2>
+<p>
+	Consiste en un evento de cordaje por equipos en el que tres charros intentan atar un toro, uno por el cuello, otro por las patas traseras, y el último luego ata sus pies juntos en un límite de tiempo máximo de 6 minutos. Se otorgan puntos por trucos de cuerda y tiempo.
+</p>
+<h2>
+	Jineteo a yegua:
+</h2><p>
+	Es lo mismo que el jineteo de toro, nada más que en este caso es un equino, pero en mi opinión siento que es más difícil ya que al tener menor, pero y una mayor destreza en sus movimientos pueden llegar a ser más bruscos.
+</p>
+<h2>
+	Manganas a pie y a caballo:
+</h2>
+<p>
+	A pie o sobre un caballo, las manganas consisten en lazar los cuartos delanteros de un cuaco (esto después de haber floreado la reata). Lo siguiente es derribar al animar ya “manganeado”. El tiempo límite son 7 minutos. La protagonista de esta faena es una yegua y los arreadores que ayudan a guiar a la yegua para que el manganeador pueda derribarla.
+</p>
+<h2>
+	Paso de la muerte:
+</h2>
+<p>
+	El Paso de la Muerte es de lo más llamativo por lo impactante que resulta a la vista, pues un charro debe saltar de un caballo a otro mientras estos van a toda velocidad para que después al estar arriba el charro dure una vuelta en el ruedo arriba de ella y finalmente bajarse sin caerse para recibir una puntuación mayor.Si el charro no llega a subirse a la yegua se le adjudica una calificación de 0 puntos.
+
+</p>
+
+<span>
+	<h2>Bibliografía</h2>
+	<ul>
+	<li> <a href="#">Link 1:</a></li>
+		<li> <a href="#">Link 2:</a></li>
+			<li> <a href="#">Link 3:</a></li>
+				<li> <a href="#">Link 4:</a></li>
+</ul>
+</span>
+
+</div>
+
+
+
+</body>
+</html>
